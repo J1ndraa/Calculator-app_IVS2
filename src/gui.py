4c6d11d6accb7421f452fab2bd681741d2494dda
Zyroshima <xsmiddo00@stud.fit.vutbr.cz>
2024-04-24 18:45:12 +0200

    added commentary to code

--- src/gui.py
+++ src/gui.py
@@ -2,94 +2,116 @@ from tkinter import *
 import tkinter as tk
 from args_handler import InputArgs
 
+# Define global variables
 BUTTON_WIDTH = 6
 BUTTON_HEIGHT = 2
 BUTTON_TEXT_SIZE = 20
 DARKBLUE = "#1167b1"
 LIGHTBLUE = "#2a9df4"
-
-
 calculation = ""
 
-def check_text():
-    return
 
+# fuction for adding character to the equation
 def append_char(parameter):
     global calculation
 
+    # only numbers and operands are allowed
     if (parameter < '0' or parameter > '9') and parameter != "+" and parameter != "-" and parameter != "*" and parameter != "/" and parameter != "^" and parameter != "%" and parameter != "(" and parameter != ")" and parameter != "." and parameter != "!" and parameter != "√":
         print("Key not allowed", parameter)
-        check_text
         return 
 
+    # if Error is already displayed, delete it
     if calculation == "Error":
         text_result.delete(1.0, "end")
         calculation = ""
 
+    # appending character (displaying in terminal for visibility)
     text_result.insert("end", parameter)
     calculation += str(parameter)
     print(calculation)
 
+
+# fuction for removing last character in equation
 def remove_char():
     print("removing char...")
     global calculation
+    
     if len(calculation) != 0:
+        # if Error is already displayed, clear the whole field
         if calculation == "Error":
-            calculation = ""
-            text_result.delete(1.0, "end")
+            clear_list()
+        # else remove the last char from string "calculation" and tkinter widget Text (named "text_result")
         else:
             calculation = calculation[:-1]
             text_result.delete("end-2c", "end")
     print(calculation)
 
+
+# function for evaluating the equation
 def export_list():
     global calculation
     if len(calculation) == 0:
-        return 
+        return
+    
+    # clear screen
     text_result.delete(1.0, "end")
-
     global result
     print("String to process:",calculation)
 
+    # run equation through our math library
     try:
         inputArgs = InputArgs(calculation)
         result = inputArgs.run_calc(3)
+    # if exception is caught, display "Error" on screen
     except:
         result = "Error"
 
+    # result is sent to our string
     calculation = str(result)
     text_result.insert(1.0, result)
 
+
+# function for clearing the whole screen
 def clear_list():
     print("Clearing input...")
     text_result.delete(1.0, "end")
     global calculation
     calculation = ""
 
+# function for pressing any key
 def key_click(event):
     append_char(event.char)
 
+# function for pressing enter
 def enter_click(event):
     export_list()
 
+# function for pressing backspace
 def backspace_click(event):
     remove_char()
 
+# function for pressing the numpad enter
 def numpad_enter_click(event):
     export_list()
 
+
+# root - main window
 root = tk.Tk()
 root.geometry("655x515")
 root.resizable(False, False)
 root.title("Klidecek Calculator")
 
+# the tkinter Text widget - used as calculator screen
 text_result = tk.Text(root, height=3, width=36, bg="white", font=("Arial", 24))
 text_result.grid(columnspan=6, pady=0, padx=0)
+
+# binding the keys
 root.bind("<Return>", enter_click)
 root.bind("<Key>", key_click)
 root.bind("<BackSpace>", backspace_click)
 root.bind("<KP_Enter>", numpad_enter_click)
 
+# configuration of all the buttons on calculator
 button_1 = tk.Button(root, text="1", command=lambda: append_char("1"), bg=DARKBLUE, fg="white", width=BUTTON_WIDTH, height=BUTTON_HEIGHT, font=("Arial", BUTTON_TEXT_SIZE,))
 button_1.grid(row=5, column=1)
 
